da172f5703d4c7c6dc866f08c18675efffa1f764
chenyangguang <vim.09071@gmail.com>
2018-02-11 23:43:05 +0800

    * change beego to iris for much faster

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	_ "github.com/chenyangguang/gosto/routers"
-	"github.com/astaxie/beego"
+	"github.com/kataras/iris"
+
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
 )
 
 func main() {
-	beego.Run()
-}
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Use(recover.New())
+	app.Use(logger.New())
 
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Hi</h1>")
+	})
+
+	app.Get("/stock", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"msg": "test other go framework"})
+	})
+
+	app.Run(iris.Addr(":9000"), iris.WithoutServerError(iris.ErrServerClosed))
+}
